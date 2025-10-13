4472fe4809de0d7d70c7fb17cd367069dfc2b8b8
Satish Samad <wnsatish@gmail.com>
2025-10-13 13:00:32 +0530

    taking inputs from textfield and showing in lazyColumn

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.EaseOutCirc
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,122 +14,210 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Surface
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+import kotlinx.serialization.descriptors.StructureKind
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    MainScreen()
+            val navController = rememberNavController()
+            val list = remember { mutableStateListOf<String>() }
+
+            NavHost(
+                navController = navController,
+                startDestination = "MainScreen"
+            ) {
+                composable("MainScreen") { MainScreen(navController, list) }
+                composable("WriteScreen") { WriteScreen(navController,list) }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavController, list: SnapshotStateList<String>) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("Notes",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontFamily = FontFamily.Serif,
+                    modifier = Modifier.shadow(8.dp)
+                    )
+            },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.DarkGray,
+                    titleContentColor = Color.White
+                )
+
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("WriteScreen")
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            items(list) { item ->
+                CustomText(item)
+            }
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    val sentences = remember { mutableStateListOf<String>() }
+fun CustomText(text: String) {
+    val myfont = FontFamily(
+        Font(R.font.myfont)
+    )
 
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp, 4.dp)
+    ) {
+        Box(modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+            contentAlignment = Alignment.Center){
+            Text(
+                text = text,
+                fontSize = 20.sp,
+                fontFamily = myfont,
+                color = Color.Black
+            )
+        }
+    }
+    Spacer(Modifier.padding(3.dp))
+}
+
+@Composable
+fun WriteScreen(navController: NavController,list: SnapshotStateList<String>) {
     var text by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TextField(
             value = text,
             onValueChange = { text = it },
-            placeholder = {Text("write something")}
+            modifier = Modifier
+                .height(400.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(5.dp)),
+            maxLines = 20,
+            placeholder = { Text("Write Something")}
         )
-        Button(
-            onClick = {
-                if(text.isNotEmpty()) {
-                    sentences.add(text)
-                    text = ""
-                }
-            }
-        ) { Text("add") }
-        TextItemView(sentences)
-    }
-    }
 
-
-@Composable
-fun TextItemView(sentence: List<String>) {
-    val customFont = Font(R.font.myboldfont)
-    val customFontFamily = FontFamily(customFont)
-
-
-    LazyColumn {
-        items(sentence) { item ->
-            Box(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .background(Color.Black)
-            ) {
-                Text(
-                    text = item,
-                    fontSize = 24.sp,
-                    fontFamily = customFontFamily,
-                    color = Color.White,
-                    modifier = Modifier.padding(8.dp)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Absolute.SpaceEvenly
+        ) {
+            Button(onClick = {
+              navController.popBackStack()
+               },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    containerColor = Color.White
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 8.dp
                 )
-            }
-        }
-    }
 
+            ) { Text("Cancel")}
+
+            Button(
+                onClick = {
+                    if(text.isNotEmpty()) {
+                        list.add(text.trim())
+                        text = ""
+                        Toast.makeText(context,"Saved",
+                            Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context,"Please write something",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) { Text("Save")}
+        }
+
+    }
 }
+
