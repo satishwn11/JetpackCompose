c9c1d5d110e33b9d36c37ab9480d372f4ec61442
Satish Samad <wnsatish@gmail.com>
2025-10-19 18:31:44 +0530

    A new MVVM random text generator feature.

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,24 +3,14 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
-import com.example.jetpackcompose.View.HomePage
-import com.example.jetpackcompose.ViewModel.HomeViewModel
+import com.example.jetpackcompose.View.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val homeViewModel = ViewModelProvider(this) [HomeViewModel::class]
-
         setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-                HomePage(modifier = Modifier.padding(paddingValues), homeViewModel)
-            }
+            MainScreen()
         }
     }
 }
--- app/src/main/java/com/example/jetpackcompose/Model/UserData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jetpackcompose.Model
-
-data class UserData(
-    val name: String,
-    val age: Int
-)
